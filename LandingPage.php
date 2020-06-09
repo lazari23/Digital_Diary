6c2c2afefe5ebd5e23351eaffed3eac87a2a8384
Ashish <ashish.shekhar@vit.edu.in>
2020-06-09 12:50:09 +0530

    Landing Page version1

---
+++ LandingPage.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Landing Page </title>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="landingpagesidebarstyle.css">
+</head>
+</head>
+<body>
+	<input type="checkbox" id="check">
+<header>
+	<label for="check">
+		<i class="fa fa-bars" aria-hidden="true" id="sidebar_btn"></i>
+	</label>
+	<div class="main">
+		<div class="logo">
+			<img src="download.png">
+		</div>
+		<ul>
+			<li class="active"><a href="Homepage.php"> Logout </a></li>			
+		</ul>
+	</div>
+	<div class="sidebar">
+		<center>
+			<img src="20190627_215946.jpg" class="profile_image">
+			<h4>Prof.Ashish Shekhar</h4><h5></br>Assistant Professor</br>Dept. of Electronics and TelecommunicationEngineering</h5>
+		</center>
+		<a href="#"><i class="fa fa-user" aria-hidden="true"></i><span>Profile</span></a>
+		<a href="#"><i class="fa fa-book" aria-hidden="true"></i><span> Academic Admin. Plan</span></a>
+		<a href="#"><i class="fa fa-table" aria-hidden="true"></i><span>Marksheets</span></a>
+		<a href="#"><i class="fa fa-line-chart" aria-hidden="true"></i><span>Status of Completion</span></a>
+	</div>
+	<div class="Faculty_Profile">
+		<h4>Qualification: ME-EXTC, Mumbai University</h4>
+		<br>
+		<h4>Paper Publications</h4>
+		<br>
+		<h4>Research Areas</h4>
+		<br>
+		<h4>Books Publications</h4>
+		<br>
+		<h4>STTP/FDP/Workshops attended </h4>
+
+	</div>
+</body>
+</html>
